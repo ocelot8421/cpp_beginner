6072ef2c9b021dad967d7dc19ef3e54060c1b219
ocelot8421 <8ocelot@gmail.com>
2023-03-12 09:36:01 +0100

    initial commit

---
+++ test.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    // std::cout<<"Hello World";
+
+    // const double pi = 3.14;
+
+    // double x = 10;
+    // int y = 3;
+    // double z = x / y;
+    // std::cout << z;
+
+    // int x = 10;
+    // int y = x++;                    // x = 11, y = 10
+    // cout << "x = " << x             //std::cout
+    //      << "; y = " << y << endl;  //stf::endl
+    // int a = 10;
+    // int z = ++a;                    // a = 11, z = 11
+    // cout << "a = " << a 
+    //      << "; z = " << z << "\n";
+
+    // cout << "Enter value: ";
+    // double value;
+    // cin >> value;
+    // cout << value;
+
+    // int number {1.2};       //give an error
+    // int number = 1.2;       //not give an error
+    // int number {};
+    // cout << number;
+
+    // int number = 0b11111111;
+    // cout << number << endl;
+    // number = 0xFF;
+    // cout << number << endl;
+    // unsigned int num = -255;    //avoid it for a while
+    // cout << num;
+
+    int number = 1'000'000;
+    cout << number << endl;
+    // short another = number;         //narrowing
+    short another {number};            //narrowing
+    cout << another;
+    return 0;
+}
